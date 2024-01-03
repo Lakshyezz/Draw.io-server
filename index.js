51aae40dda282df1cea9a9b4796848ba08c5cf30
lakshay deep <deep.lakshya1310@gmail.com>
2024-01-04 03:17:04 +0530

    server

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+const server = createServer(app);
+app.use(cors({origin: "http://localhost:3000/"}));
+
+const io = new Server(server,{ cors : "http://localhost:3000/" }); // from socket.io
+
+
+io.on('connection' , (socket)=>{
+    // console.log('server connected');
+
+    socket.on('beginPath', (arg) =>{
+        socket.broadcast.emit('beginPath',arg)
+    });
+    socket.on('drawLine', (arg) =>{
+        socket.broadcast.emit('drawLine',arg)
+    })
+    socket.on('changeConfig', (arg) =>{
+        socket.broadcast.emit('changeConfig',arg)
+    })
+});
+
+
+
+server.listen(5000, () => {
+  console.log('server running at http://localhost:5000');
+});
